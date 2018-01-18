dd9008c401e4e6d23acfe829fcb3f6c9f04aba3a
ErikBoesen <me@erikboesen.com>
2018-01-18 08:41:03 -0500

    Reorganize inputs and seed data into objects

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 var tbody = document.getElementById('tbody'),
-    xi = document.getElementById('xi'),
-    yi = document.getElementById('yi'),
-    dydx = document.getElementById('dydx'),
-    dx = document.getElementById('dx'),
-    iterations = document.getElementById('iterations'),
+    inputs = {
+        xi: document.getElementById('xi'),
+        yi: document.getElementById('yi'),
+        dydx: document.getElementById('dydx'),
+        dx: document.getElementById('dx'),
+        iterations: document.getElementById('iterations'),
+    },
     update = document.getElementById('update');
 
 oninput = function() {
@@ -11,27 +13,31 @@ oninput = function() {
 };
 
 update.onclick = function() {
-    var x = parseFloat(xi.value);
-    var y = parseFloat(yi.value);
-    var eqn = dydx.value.replace(/(\d)([a-z])/g, '$1*$2')
-                        .replace(/([a-z])([a-z])/g, '$1*$2')
-                        .replace(/([a-z])([a-z])/g, '$1*$2')
-                        .replace(/([a-z])(\d)/, '$1*$2')
-                        .replace('^', '**')
-                        .replace(/([a-z])/g, 'parseFloat($1)');
+    var x = parseFloat(inputs.xi.value),
+        y = parseFloat(inputs.yi.value),
+        seed = {
+            dydx: inputs.dydx.value.replace(/(\d)([a-z])/g, '$1*$2')
+                                   .replace(/([a-z])([a-z])/g, '$1*$2')
+                                   .replace(/([a-z])([a-z])/g, '$1*$2')
+                                   .replace(/([a-z])(\d)/, '$1*$2')
+                                   .replace('^', '**')
+                                   .replace(/([a-z])/g, 'parseFloat($1)'),
+            dx: inputs.dx.value,
+            iterations: inputs.iterations.value,
+        };
     while (tbody.firstChild) tbody.removeChild(tbody.firstChild);
-    for (i = 0; i < iterations.value; i++) {
+    for (i = 0; i < seed.iterations; i++) {
         var tr = document.createElement('tr');
         var tds = [];
         for (ii = 0; ii < 5; ii++) tds[ii] = document.createElement('td');
         tds[0].textContent = '(' + pretty(x) + ', ' + pretty(y) + ')';
-        var deriv = eval(eqn); // Yes. Yes, I know.
+        var deriv = eval(seed.dydx); // Yes. Yes, I know.
         tds[1].textContent = pretty(deriv);
-        tds[2].textContent = dx.value;
-        var increase = deriv * dx.value;
-        tds[3].textContent = pretty(increase);
-        x += parseFloat(dx.value);
-        y += increase;
+        tds[2].textContent = seed.dx;
+        var dy = deriv * seed.dx;
+        tds[3].textContent = pretty(dy);
+        x += parseFloat(seed.dx);
+        y += dy;
         tds[4].textContent = '(' + pretty(x) + ', ' + pretty(y) + ')';
         for (ii = 0; ii < 5; ii++) tr.appendChild(tds[ii]);
         tbody.appendChild(tr);
